993056e6e4e41ea4cc154dcfbf0857b64c6094c8
Madhavir123 <22eg106b35@anurag.edu.in>
2024-07-13 11:44:51 -0700

    first commit

---
+++ app_server/controllers/dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var index = function index(req, res) {
+  res.render('index', {
+    title: 'Express'
+  });
+};
+
+module.exports = {
+  index: index
+};
+//# sourceMappingURL=main.dev.js.map
---
+++ app_server/controllers/main.js
@@ -0,0 +1,6 @@
+const index = (req, res) => {
+    res.render('index', { title: 'Express' });
+    };
+    module.exports = {
+        index
+        };
